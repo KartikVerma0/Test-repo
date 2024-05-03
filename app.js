88ed08e0a7d1e2a80c13c51d4e65b1e7cb9b64f7
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-03 11:50:51 +0530

    Added express, .gitignore, configured actions for self hosted servers

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.send("hello")
+})
+
+app.listen(() => {
+    console.log("Server running on 7000")
+}, 7000)
